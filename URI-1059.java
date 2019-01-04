9011ccb756e25e5be459246278b3263bafa56d0d
NS Sizan <45298845+sizan123@users.noreply.github.com>
2019-01-05 02:21:37 +0600

    Create URI-1059.java

---
+++ URI-1059.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args) {
+     
+        for (int i = 2; i <=100; i++) {
+            
+            if (i%2==0) {
+                
+                System.out.println(i);
+                
+            }
+            
+        }
+        
+    }
+    
+}
