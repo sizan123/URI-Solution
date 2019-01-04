d6e9e4c258bfb1aef0275eb5dc595093e6d8f98a
NS Sizan <45298845+sizan123@users.noreply.github.com>
2019-01-05 02:35:06 +0600

    Create URI-1145.java

---
+++ URI-1145.java
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int X,Y,i,j=0;
+    scanf("%d%d", &X,&Y);
+    for(i=1; i<=Y; i++)
+    {
+        j++;
+        if(j==X) printf("%d",i);
+        else printf("%d ", i);
+        if(j==X)
+        {
+            j=0;
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
