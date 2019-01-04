2d054673a7c10e1889901c4c7d0ba7d85c5916f1
NS Sizan <45298845+sizan123@users.noreply.github.com>
2019-01-05 02:39:42 +0600

    Create URI-1041.java

---
+++ URI-1041.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        float x, y;
+
+        x = in.nextFloat();
+        y = in.nextFloat();
+
+        if (x == 0.0 && y != 0.0) {
+            System.out.println("Eixo Y");
+        } else if (x != 0.0 && y == 0.0) {
+            System.out.println("Eixo X");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        } else if (x == 0.0 && y == 0) {
+            System.out.println("Origem");
+        }
+    }
+
+}
